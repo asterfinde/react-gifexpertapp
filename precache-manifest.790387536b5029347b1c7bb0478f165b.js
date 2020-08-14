2dc030dee74ba893bfff99d5a1ab5e19d34752a9
asterfinde <dante.frias@gmail.com>
2020-08-14 12:01:13 -0500

    First commit

---
+++ precache-manifest.790387536b5029347b1c7bb0478f165b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b466e00f37fec63f028d28bf282a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3729bcbe037192d8016c",
+    "url": "/static/css/main.8f87b048.chunk.css"
+  },
+  {
+    "revision": "80388b3f083f7dd673cf",
+    "url": "/static/js/2.6d56e17c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d56e17c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3729bcbe037192d8016c",
+    "url": "/static/js/main.69c91ed1.chunk.js"
+  },
+  {
+    "revision": "a70e4ae6bdd791779146",
+    "url": "/static/js/runtime-main.1e57df78.js"
+  }
+]);
